3a9fbcaf80fec0d70ed29ba761a12f444cca2715
Adrian <adrian>
2024-03-19 23:48:45 +0100

    Mensaje del commit

--- back/src/models/Pokemodels.js
+++ back/src/models/Pokemodels.js
@@ -1,39 +1,44 @@
 const mongoose = require('mongoose');
 
-// Definimos el modelo de producto
-const productSchema = new mongoose.Schema({
-    nombre: {
+
+// Definir el esquema para el modelo de Pokémon
+const PokemonSchema = new Schema({
+    nombre: { 
         type: String,
-        required: true
-    },
-    descripcion: {
-        type: String,
-        required: true
-    },
-    imagen: {
-        type: String,
-        required: true
-    },
-    tipo: {
-        type: [String],
-        enum: ['Camisetas', 'Pantalones', 'Zapatos', 'Accesorios'],
-        required: true
-    },
-    talla: {
-        type: String,
-        enum: ['XS', 'S', 'M', 'L', 'XL'],
-        required: true
-    },
-    precio: {
+         required: true 
+        },
+    tipo: [
+        { type: String,
+        enum: ['Normal', 'Fuego', 'Agua', 'Planta', 'Eléctrico', 'Hielo', 'Lucha', 'Veneno', 'Tierra',
+        'Volador', 'Psíquico', 'Bicho', 'Roca', 'Fantasma', 'Dragón', 'Siniestro', 'Acero', 'Hada']
+        }],
+    peso: {
         type: Number,
-        required: true
-    }
+        required: true 
+    },
+    altura: { 
+        type: Number,
+         required: true 
+        },
+    descripcion: String,
+    estadisticas: {
+        type: Map,
+        of: {
+            nombre: String,
+            valor: Number
+        }
+    },
+    id_pokedex: Number,
+    legendario: { 
+        type: Boolean, 
+        default: false 
+    },
+    base_experience: Number,
+    habilidad: String,
+    precio:Number
 });
 
+// Crear el modelo de Pokémon
+const Pokemon = mongoose.model('Pokemon', PokemonSchema);
 
-
-// Crear el modelo de Producto
-const Product = mongoose.model('Product', productSchema);
-
-//exportamos el modelo de Producto
-module.exports = Product;
+module.exports = Pokemon;
