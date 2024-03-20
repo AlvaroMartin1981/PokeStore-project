966364279e4b1c953bfd0ae8b87227f9b3d7e4c5
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-03-20 14:15:01 +0100

    Update fetchPoke.js

--- back/src/models/fetchPoke.js
+++ back/src/models/fetchPoke.js
@@ -1,11 +1,1 @@
-const ObtenerPokemon = async ()=>{
-try {
-    const response = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-    const data = await response.json()
-    console.log(data)
 
-} catch (error) {
-    console.log(error)
-
-}}
-ObtenerPokemon()
