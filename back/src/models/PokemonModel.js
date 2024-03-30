269c1b7881e1845a724cc0735d9e0d7ec8705bec
Adrian <adrian>
2024-03-30 04:13:28 +0100

    create controller poke y balls

--- back/src/models/PokemonModel.js
+++ back/src/models/PokemonModel.js
@@ -1,51 +1,82 @@
 const mongoose = require('mongoose');
 
-
-// Definir el esquema para el modelo de Pokémon
 const PokemonSchema = new mongoose.Schema({
-    nombre: { 
-        type: String,
-         required: true 
-        },
-
-    tipo: [{
-         type: String
-        }],
-    peso: {
-        type: Number,
-        required: true 
-
-    },
-    altura: { 
-        type: Number,
-         required: true 
-        },
-    imagen:{
-        type:String,
-        required:true
-    },
-    habilidades: [{
+  nombre: {
+    type: String,
+    required: true,
+  },
+  tipo: {
+    type: [String],
+    required: true,
+  },
+  peso: {
+    type: Number,
+    required: true,
+  },
+  altura: {
+    type: Number,
+    required: true,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  estadisticas: {
+    type: [
+      {
         nombre: String,
-        descripcion:String
-    }],
-    descripcion: String,
-    estadisticas: [{
-            nombre: String,
-            valor: Number
-        }],
-    id_pokedex: Number,
-    legendario: { 
-        type: Boolean, 
-        default: false 
-    },
-    mythical: { 
-        type: Boolean, 
-        default: false 
-    },
-    ratio_captura:Number,
-    base_experience: Number,
-    precio:Number
+        valor: Number,
+      },
+    ],
+    required: true,
+  },
+  id_pokedex: {
+    type: Number,
+    required: true,
+  },
+  legendario: {
+    type: Boolean,
+    required: true,
+  },
+  mythical: {
+    type: Boolean,
+    required: true,
+  },
+  habilidades: {
+    type: [
+      {
+        nombre: String,
+        descripcion: String,
+      },
+    ],
+    required: true,
+  },
+  ratio_captura: {
+    type: Number,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  base_experience: {
+    type: Number,
+  },
+  cadena_evoluciones: {
+    type: [
+      {
+        especie: String,
+        nivel: Number,
+      },
+    ],
+  },
+  evolucionDe: String,
 });
+
 
 // Crear el modelo de Pokémon
 const PokemonModel = mongoose.model('Pokemon', PokemonSchema);
