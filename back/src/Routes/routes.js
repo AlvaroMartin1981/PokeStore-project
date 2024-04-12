de758f62b8f8b3ae8c24789cb94e16b3c511f4a4
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2024-04-12 20:42:04 +0000

    merge

--- back/src/Routes/routes.js
+++ back/src/Routes/routes.js
@@ -4,15 +4,11 @@ const itemController = require('../controllers/itemController');
 const pokemonController = require('../controllers/pokemonController');
 const pokeballController = require('../controllers/pokeballController')
 
-
 //const productController= require('../controllers/productController')
-
-
 
 routes.get( '/', (req, res) => {
     res.redirect('/products/')
 });
-
 
 routes.get('/pokemon',pokemonController.showPokemon)
 routes.get('/pokemon/nombre/:nombre',pokemonController.pokemonPorNombre)
@@ -31,8 +27,5 @@ routes.get('/items/categoria/:itemstipo',itemController.itemsTipo)//revisar
 routes.get('/items/:itemsId',itemController.itemsId)//revisar
 routes.get('/items/nombre/:nombre',itemController.itemsPorNombre)
 
-//Falta buscar items por nombre y por tipo
-
-//ademas de las rutas de dashboard y de usuario
 
 module.exports= routes 
